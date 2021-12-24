8908ea5f2b556765b9e6bab2b171844d678d1d92
artur_hurynovich <artur.hurynovich@gmail.com>
2021-12-24 21:31:59 +0300

    Initial commit

---
+++ src/main/java/com/hurynovich/data_unit_schema_service/DataUnitSchemaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hurynovich.data_unit_schema_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataUnitSchemaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataUnitSchemaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hurynovich/data_unit_schema_service/DataUnitSchemaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hurynovich.data_unit_schema_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataUnitSchemaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
